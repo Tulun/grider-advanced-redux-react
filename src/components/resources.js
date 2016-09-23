60aa3337792acc4f00be9b7c5ffa25aa82422fba
Jason Kiraly <jaykiraly@gmail.com>
2016-09-23 19:17:07 +0000

    Added a basic resources component and defined the route.

---
+++ src/components/resources.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default () => {
+    return (
+        <div>
+            Super special secret recipe
+            <ul>
+                <li> 1 cup sugar</li>
+                <li> 1 cup salt </li>
+            </ul>
+        </div>
+    )
+}
